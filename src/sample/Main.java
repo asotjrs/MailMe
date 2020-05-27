c6c8ab2e559c78f9daf8b6b74daedbf34dec72ae
unknown <asotjrs@gmail.com>
2020-05-27 19:41:57 +0200

    just changing the title with fixing some db problems

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
         Parent root = fxmlLoader.load();
-        primaryStage.setTitle("INTTIC Contacts");
+        primaryStage.setTitle("Mail Me");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
 
